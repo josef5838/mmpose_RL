1b40830ea09e94bd7a43abc7214c6af1c95140c7
Robot Learning Course exercise account <rl_course_22@snowflake02.ee.ethz.ch>
2024-05-13 22:06:50 +0200

    adjusted bash script

--- tools/sbatch_train.sh
+++ tools/sbatch_train.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --account=rl
 #SBATCH --cpus-per-task=4
-#SBATCH --time=$5:00:00
+#SBATCH -t 6:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --tmp=50000                        # per node!!
 #SBATCH --job-name=$3
 #SBATCH --output=$3.out
-#SBATCH --gpus=$2
-#SBATCH --gres=gpumem:10G
+#SBATCH --gres=gpu:$2  
+
+
+
 
 CONFIG=$1
 GPUS=$2
 NAME=$3
-TIME=$4
-WORKDIR=$5
+WORKDIR=$4
 
 tools/dist_train.sh $CONFIG $GPUS --work-dir $WORKDIR
